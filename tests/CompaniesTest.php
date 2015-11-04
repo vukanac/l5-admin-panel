a8054f76ef2d257f50676fedb12ff283718c1772
Vladimir Vukanac <vladimir@vukanac.com>
2015-11-04 21:22:15 +0100

    Add more tests for Company

--- tests/CompaniesTest.php
+++ tests/CompaniesTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use App\Company;
 
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -10,7 +11,9 @@ class CompaniesTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function test_tasks_are_displayed_on_the_dashboard()
+
+    
+    public function test_companies_are_displayed_on_the_dashboard()
     {
         factory(Company::class)->create(['name' => 'Company 1']);
         factory(Company::class)->create(['name' => 'Company 2']);
@@ -23,7 +26,7 @@ class CompaniesTest extends TestCase
     }
 
 
-    public function test_tasks_can_be_created()
+    public function test_companies_can_be_created()
     {
         $this->visit('/')->dontSee('Company 1');
 
@@ -34,7 +37,7 @@ class CompaniesTest extends TestCase
     }
 
 
-    public function test_long_tasks_cant_be_created()
+    public function test_long_companies_cant_be_created()
     {
         $this->visit('/')
             ->type(str_random(300), 'name')
@@ -135,4 +138,83 @@ class CompaniesTest extends TestCase
              ;
 
     }
+    public function test_i_am_redirect_to_login_if_i_try_to_view_company_lists_without_logging_in()
+    {
+        $this->visit('/companies')->see('Login');
+    }
+
+
+    public function test_i_can_create_an_account()
+    {
+        $this->visit('/auth/register')
+            ->type('Taylor Otwell', 'name')
+            ->type('taylor@laravel.com', 'email')
+            ->type('secret', 'password')
+            ->type('secret', 'password_confirmation')
+            ->press('Register')
+            ->seePageIs('/companies')
+            ->seeInDatabase('users', ['email' => 'taylor@laravel.com']);
+    }
+
+
+    public function test_authenticated_users_can_create_companies()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('/companies')
+             ->type('Company 1', 'name')
+             ->press('Add Company')
+             ->see('Company 1')
+             ->seeInDatabase('companies', ['name' => 'Company 1']);
+    }
+
+
+    public function test_users_can_delete_a_company()
+    {
+        $user = factory(User::class)->create();
+
+        $user->companies()->save($companyOne = factory(Company::class)->create());
+        $user->companies()->save($companyTwo = factory(Company::class)->create());
+
+        $this->actingAs($user)
+             ->visit('/companies')
+             ->see($companyOne->name)
+             ->see($companyTwo->name)
+             ->press('delete-company-'.$companyOne->id)
+             ->dontSee($companyOne->name)
+             ->see($companyTwo->name);
+    }
+
+
+    public function test_users_cant_view_companies_of_other_users()
+    {
+        $userOne = factory(User::class)->create();
+        $userTwo = factory(User::class)->create();
+
+        $userOne->companies()->save($companyOne = factory(Company::class)->create());
+        $userTwo->companies()->save($companyTwo = factory(Company::class)->create());
+
+        $this->actingAs($userOne)
+             ->visit('/companies')
+             ->see($companyOne->name)
+             ->dontSee($companyTwo->name);
+    }
+
+
+    public function test_users_cant_delete_companies_of_other_users()
+    {
+        $this->withoutMiddleware();
+
+        $userOne = factory(User::class)->create();
+        $userTwo = factory(User::class)->create();
+
+        $userOne->companies()->save($companyOne = factory(Company::class)->create());
+        $userTwo->companies()->save($companyTwo = factory(Company::class)->create());
+
+        $this->actingAs($userOne)
+             ->delete('/company/'.$companyTwo->id)
+             ->assertResponseStatus(403);
+    }
+
 }
