c109e9ff50f0b24da87f3e67bdaab0b905cc8e21
Vladimir Vukanac <vladimir@vukanac.com>
2015-11-06 22:26:21 +0100

    Add Auth middleware to Company Controller

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -10,6 +10,10 @@ use App\Http\Controllers\Controller;
 
 class CompanyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
