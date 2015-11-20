870a4c79b3113e87c1c2da931834daac356708ec
Vladimir Vukanac <vukanac@gmail.com>
2015-11-20 15:55:06 +0100

    Fix list companies layout

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -3,68 +3,67 @@
 @section('content')
     <div class="container">
         <div class="col-sm-offset-2 col-sm-8">
+
             <div class="panel panel-default">
                 <div class="panel-heading">
                     New Company
                 </div>
+                <div class="panel-body">
+                    <!-- Display Validation Errors -->
+                    @include('common.errors')
 
-    <!-- Bootstrap Boilerplate... -->
+                    @can('create-company')
+                    <!-- New Company Form -->
+                    <form action="/company" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
 
-    <div class="panel-body">
-        <!-- Display Validation Errors -->
-        @include('common.errors')
+                        <!-- Company Name -->
+                        <div class="form-group">
+                            <label for="company" class="col-sm-3 control-label">Company</label>
 
-        @can('create-company')
-        <!-- New Company Form -->
-        <form action="/company" method="POST" class="form-horizontal">
-            {{ csrf_field() }}
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="company-name" class="form-control">
+                            </div>
+                        </div>
 
-            <!-- Company Name -->
-            <div class="form-group">
-                <label for="company" class="col-sm-3 control-label">Company</label>
-
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="company-name" class="form-control">
+                        <!-- Add Company Button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-plus"></i> Add Company
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    @else
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <p>User is not authorised to Create Company.</p>
+                        </div>
+                    </div>
+                    @endcan
                 </div>
             </div>
 
-            <!-- Add Company Button -->
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Add Company
-                    </button>
+            <!-- Current Companies -->
+            @if (count($companies) > 0)
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    List of companies
                 </div>
-            </div>
-        </form>
-        @else
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <p>User is not authorised to Create Company.</p>
-            </div>
-        </div>
-        @endcan
-    </div>
 
-    <!-- Current Companies -->
-    @if (count($companies) > 0)
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                List of companies
-            </div>
+                <div class="panel-body">
+                    <table class="table table-striped company-table">
 
-            <div class="panel-body">
-                <table class="table table-striped company-table">
+                        <!-- Table Headings -->
+                        <thead>
+                            <th>Company</th>
+                            <th>&nbsp;</th>
+                        </thead>
 
-                    <!-- Table Headings -->
-                    <thead>
-                        <th>Company</th>
-                        <th>&nbsp;</th>
-                    </thead>
-
-                    <!-- Table Body -->
-                    <tbody>
-                        @foreach ($companies as $company)
+                        <!-- Table Body -->
+                        <tbody>
+                            @foreach ($companies as $company)
                             <tr>
                                 <!-- Company Name -->
                                 <td class="table-text">
@@ -77,9 +76,8 @@
                                         @endcan
                                     </div>
                                 </td>
-
-                                @can('update-company', $company)
                                 <td>
+                                    @can('update-company', $company)
                                     <!-- Edit Button -->
                                     <form action="{{ url('company/edit/'.$company->id) }}" method="GET">
                                         {{ csrf_field() }}
@@ -87,8 +85,10 @@
 
                                         <button id="edit-company-{{ $company->id }}">Edit</button>
                                     </form>
-                                @endcan
-                                @can('destroy-company', $company)
+                                    @else
+                                        &nbsp;
+                                    @endcan
+                                    @can('destroy-company', $company)
                                     <!-- Delete Button -->
                                     <form action="/company/{{ $company->id }}" method="POST">
                                         {{ csrf_field() }}
@@ -96,16 +96,17 @@
 
                                         <button id="delete-company-{{ $company->id }}">Delete Company</button>
                                     </form>
+                                    @else
+                                        &nbsp;
+                                    @endcan
                                 </td>
-                                @endcan
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-    @endif
+            @endif
         </div>
     </div>
-
 @endsection
