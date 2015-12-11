deb6f3a7c12eae522f602ebd2d55498b227b03cb
Vladimir Vukanac <vukanac@gmail.com>
2015-12-11 23:04:19 +0100

    Add company approval email template

---
+++ resources/views/emails/company-approval.blade.php
@@ -0,0 +1,10 @@
+<!-- resources/views/emails/company-approval.blade.php -->
+
+Your Application Licence has bin approved.
+<br/>
+<br/>To login and start using application go to:
+<br/>
+<br/><a href="{{ url('auth/login') }}">{{ url('auth/login') }}</a>
+<br/>
+<br/>Email sent by
+<br/>Licence Manager
