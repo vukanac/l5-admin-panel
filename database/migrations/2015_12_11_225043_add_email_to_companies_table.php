c549ee092804cada385cd1bc8f864dd60d6e27cc
Vladimir Vukanac <vukanac@gmail.com>
2015-12-12 00:02:27 +0100

    Add email field for company

---
+++ database/migrations/2015_12_11_225043_add_email_to_companies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddEmailToCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->string('email')->nullable()->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            if (Schema::hasColumn('companies', 'email')) {
+                $table->dropColumn('email');
+            }
+        });
+    }
+}
