4ee9d287fcb1d7d3c0d552a94c4158e11f920bf8
Vladimir Vukanac <vukanac@gmail.com>
2016-01-12 18:05:22 +0100

    Log every scheduled actions start, finish and results

--- app/Model/ActionQueue/ActionCommandScheduled.php
+++ app/Model/ActionQueue/ActionCommandScheduled.php
@@ -2,14 +2,20 @@
 
 namespace App\Model\ActionQueue;
 
+use Log;
 
 class ActionCommandScheduled
 {
 
     public static function run()
     {
+    	Log::info('Schedule run start.');
+
         $client = new ActionCommandClient();
         $result = $client->run();
+
+        Log::info('Schedule result.', $result);
+        Log::info('Schedule run finish.');
 
         return json_encode($result);
     }
